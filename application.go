755e7dee2286628d23403f81d53d9dfd217f0cd3
double <2637309949@qq.com>
2019-04-15 16:31:46 +0800

    add rota file

--- application.go
+++ application.go
@@ -9,46 +9,28 @@
 package logger
 
 import (
-	"fmt"
 	"path"
 	"time"
 
 	"github.com/2637309949/bulrush"
+	journal "github.com/2637309949/bulrush-addition/logger"
 	"github.com/gin-gonic/gin"
 )
 
-// LOGLEVEL -
-type LOGLEVEL int
-
-const (
-	// SYSLEVEL  -
-	SYSLEVEL LOGLEVEL = 0 + iota
-	// USERLEVEL -
-	USERLEVEL
-	// SYSSTROBE -
-	SYSSTROBE = 1024 * 1024 * 5
-	// USERSTROBE -
-	USERSTROBE = 1024 * 1024 * 5
-)
-
 type (
-	// LoggerWriter plugin
-	LoggerWriter struct {
+	// Logger plugin
+	Logger struct {
 		bulrush.PNBase
 		cfg *bulrush.Config
 	}
 )
 
 // Plugin for Recovery
-func (logger *LoggerWriter) Plugin() bulrush.PNRet {
+func (logger *Logger) Plugin() bulrush.PNRet {
 	return func(cfg *bulrush.Config, router *gin.RouterGroup) {
 		logger.cfg = cfg
+		journal := journal.CreateHTTPLogger(path.Join(".", Some(LeftV(cfg.String("logs")), "logs").(string)))
 		router.Use(func(c *gin.Context) {
-			fmt.Println("####  logger")
-			logsDir := Some(LeftV(cfg.String("logs")), "logs").(string)
-			logsDir = path.Join(".", logsDir)
-			logPath := getLogFile(SYSLEVEL, logsDir)
-			writer := createLog(logPath)
 			start := time.Now()
 			path := c.Request.URL.Path
 			raw := c.Request.URL.RawQuery
@@ -57,25 +39,11 @@ func (logger *LoggerWriter) Plugin() bulrush.PNRet {
 			}
 			clientIP := c.ClientIP()
 			method := c.Request.Method
-
-			fmt.Fprintf(writer, "[%v] => %s %6s %s\n", start.Format("2006/01/02 15:04:05"), clientIP, method, path)
+			journal.Info("[%v] => %s %6s %s\n", start.Format("2006/01/02 15:04:05"), clientIP, method, path)
 			c.Next()
 			end := time.Now()
 			latency := float64(end.Sub(start) / time.Millisecond)
-			fmt.Fprintf(writer, "[%v] <= %.2fms %s %6s %s\n", end.Format("2006/01/02 15:04:05"), latency, clientIP, method, path)
+			journal.Info("[%v] <= %.2fms %s %6s %s\n", end.Format("2006/01/02 15:04:05"), latency, clientIP, method, path)
 		})
 	}
-}
-
-// Writer -
-// fileName start with "1"
-// User level
-func (logger *LoggerWriter) Writer(info string) {
-	logsDir := Some(LeftV(logger.cfg.String("logs")), "logs").(string)
-	logsDir = path.Join(".", logsDir)
-	logPath := getLogFile(USERLEVEL, logsDir)
-	writer := createLog(logPath)
-	out := writer
-	start := time.Now()
-	fmt.Fprintf(out, "[%v]<-S-> %s\n", start.Format("2006/01/02 15:04:05"), info)
 }
--- utils.go
+++ utils.go
@@ -8,16 +8,6 @@
 
 package logger
 
-import (
-	"fmt"
-	"io"
-	"os"
-	"path"
-	"path/filepath"
-	"strings"
-	"time"
-)
-
 // Some get or a default value
 func Some(target interface{}, initValue interface{}) interface{} {
 	if target != nil && target != "" && target != 0 {
@@ -29,56 +19,4 @@ func Some(target interface{}, initValue interface{}) interface{} {
 // LeftV -
 func LeftV(left interface{}, right interface{}) interface{} {
 	return left
-}
-
-// createLog -
-func createLog(path string) io.Writer {
-	f, _ := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0600)
-	writer := io.MultiWriter(f, os.Stdout)
-	return writer
-}
-
-// getLogFile -
-func getLogFile(level LOGLEVEL, basePath string) string {
-	var filePath string
-	levelStr := levelStr(level)
-	filepath.Walk(basePath, func(path string, info os.FileInfo, err error) error {
-		if filePath != "" {
-			return nil
-		}
-		fileName := info.Name()
-		fileSize := info.Size()
-		levelMatch := strings.HasPrefix(fileName, levelStr)
-		sizeMatch := false
-		if level == SYSLEVEL {
-			sizeMatch = fileSize < SYSSTROBE
-		} else if level == USERLEVEL {
-			sizeMatch = fileSize < USERSTROBE
-		}
-
-		if levelMatch && sizeMatch {
-			filePath = path
-		}
-		return nil
-	})
-	if filePath != "" {
-		return filePath
-	}
-	// create level log file
-	fileName := time.Now().Format("2006.01.02 15.04.05")
-	fileName = fmt.Sprintf("%s_"+fileName+".log", levelStr)
-	filePath = path.Join(basePath, fileName)
-	return filePath
-}
-
-// levelStr -
-func levelStr(level LOGLEVEL) string {
-	var levelStr string
-	switch level {
-	case SYSLEVEL:
-		levelStr = "0"
-	case USERLEVEL:
-		levelStr = "1"
-	}
-	return levelStr
 }
