93a8b2f25dbc5bf2fe1b87066630f6d5f91dca28
double <2637309949@qq.com>
2019-04-28 16:31:31 +0800

    优化

--- application.go
+++ application.go
@@ -9,6 +9,7 @@
 package logger
 
 import (
+	"fmt"
 	"path"
 	"time"
 
@@ -18,32 +19,64 @@ import (
 )
 
 type (
+	// Payload in request
+	Payload struct {
+		StartUnix int64
+		EndUnix   int64
+		Type      string
+		Latency   float64
+		Method    string
+		IP        string
+		URL       string
+	}
+
 	// Logger plugin
 	Logger struct {
 		bulrush.PNBase
-		Path string
+		Format func(*Payload, *gin.Context) string
+		Path   string
 	}
 )
+
+func defaultFormat(p *Payload, ctx *gin.Context) string {
+	if p.Type == "IN" {
+		startTime := time.Unix(p.StartUnix, 0).Format("2006/01/02 15:04:05")
+		return fmt.Sprintf("[%v] => %s %6s %s", startTime, p.IP, p.Method, p.URL)
+	} else if p.Type == "OUT" {
+		endOfTime := time.Unix(p.EndUnix, 0).Format("2006/01/02 15:04:05")
+		latency := float64(time.Unix(p.EndUnix, 0).Sub(time.Unix(p.StartUnix, 0)) / time.Millisecond)
+		return fmt.Sprintf("[%v] <= %.2fms %s %6s %s", endOfTime, latency, p.IP, p.Method, p.URL)
+	}
+	return "FROMAT ERROR"
+}
 
 // Plugin for Recovery
 func (logger *Logger) Plugin() bulrush.PNRet {
 	return func(cfg *bulrush.Config, router *gin.RouterGroup) {
 		logger.Path = Some(logger.Path, LeftV(cfg.String("logs"))).(string)
 		journal := journal.CreateHTTPLogger(path.Join(".", Some(logger.Path, "logs").(string)))
+		payload := &Payload{}
+		if logger.Format == nil {
+			logger.Format = defaultFormat
+		}
 		router.Use(func(c *gin.Context) {
-			start := time.Now()
 			path := c.Request.URL.Path
 			raw := c.Request.URL.RawQuery
 			if raw != "" {
 				path = path + "?" + raw
 			}
-			clientIP := c.ClientIP()
-			method := c.Request.Method
-			journal.Info("[%v] => %s %6s %s", start.Format("2006/01/02 15:04:05"), clientIP, method, path)
+			payload.Type = "IN"
+			payload.StartUnix = time.Now().Unix()
+			payload.IP = c.ClientIP()
+			payload.Method = c.Request.Method
+			payload.URL = path
+			in := logger.Format(payload, c)
+			journal.Info("%s", in)
 			c.Next()
-			end := time.Now()
-			latency := float64(end.Sub(start) / time.Millisecond)
-			journal.Info("[%v] <= %.2fms %s %6s %s", end.Format("2006/01/02 15:04:05"), latency, clientIP, method, path)
+			payload.Type = "OUT"
+			payload.EndUnix = time.Now().Unix()
+			out := logger.Format(payload, c)
+			journal.Info("%s", out)
 		})
 	}
 }
